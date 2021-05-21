8b1141f41d71ba88fcff9f463f891293bc5f8d22
genadi53 <kotka5353@gmail.com>
2021-05-21 14:19:12 +0300

    initial commit

---
+++ src/homepage.component.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const HomePage = () => {
+    return (
+        <div className="homepage">
+          <div className="directory-name">
+            <div className="menu-item">
+              <div className="content">
+                <h1 className="title">Hats</h1>
+                <span className="subtitle">Shop now</span>
+              </div>
+            </div>
+          </div>
+        </div>
+      );
+}
+
+export default HomePage;
