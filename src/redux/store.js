e74afbe39540a01f192c697fb48480f5a2bbcb31
genadi53 <kotka5353@gmail.com>
2021-05-29 18:43:04 +0300

    implenent redux-persist

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,12 +1,15 @@
 import { createStore, applyMiddleware } from 'redux';
+import { persistStore } from 'redux-persist';
 import logger from 'redux-logger';
 
 import rootReducer from './root-reducer';
 
 const middlewares = [logger];
+
 const store = createStore(rootReducer, 
     window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-    ); //applyMiddleware(...middlewares)
+); //applyMiddleware(...middlewares)
 
+const persistor = persistStore(store);
 
-export default store;
+export { store, persistor };
