8345dc4e774cfd8bf673211e1b46c4bace5ed0d1
genadi53 <kotka5353@gmail.com>
2021-05-29 14:56:36 +0300

    add ability to add/remove items from checkout

--- src/redux/cart/cart.utils.js
+++ src/redux/cart/cart.utils.js
@@ -12,3 +12,21 @@ export const addItemToCart = (cartItems, cartItemToAdd) => {
 
     return [...cartItems, { ...cartItemToAdd, quantity: 1 }];
 }
+
+export const removeItemFromCart = (cartItems, cartItemToRemove) => {
+    const existingCartItem = cartItems.find(
+        item => item.id === cartItemToRemove.id
+    );
+
+    if(existingCartItem.quantity === 1) {
+        return cartItems.filter(item => 
+            item.id !== cartItemToRemove.id  
+        );
+
+    } else {
+        return cartItems.map(item => 
+            item.id === cartItemToRemove.id ?
+            { ...item, quantity: item.quantity -1 } : item   
+        );
+    }
+}
