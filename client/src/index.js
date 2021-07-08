d3f5da9ae9c07a0f01ef71051442b5aabf91c210
genadi53 <kotka5353@gmail.com>
2021-07-08 14:06:06 +0300

    update app to be pwa compliant and added serviceworker

--- client/src/index.js
+++ client/src/index.js
@@ -1,33 +1,33 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { BrowserRouter } from 'react-router-dom';
-import { Provider } from 'react-redux';
-import { PersistGate } from 'redux-persist/integration/react';
+import React from "react";
+import ReactDOM from "react-dom";
+import { BrowserRouter } from "react-router-dom";
+import { Provider } from "react-redux";
+import { PersistGate } from "redux-persist/integration/react";
 
-import { store, persistor } from './redux/store';
+import { store, persistor } from "./redux/store";
 
-import './index.css';
-import App from './App';
-// import * as serviceWorkerRegistration from './serviceWorkerRegistration';
+import "./index.css";
+import App from "./App";
+import * as serviceWorkerRegistration from "./serviceWorkerRegistration";
 // import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
-  <Provider store={ store }>
+  <Provider store={store}>
     <BrowserRouter>
       <React.StrictMode>
-        <PersistGate persistor={ persistor }>
+        <PersistGate persistor={persistor}>
           <App />
         </PersistGate>
       </React.StrictMode>
     </BrowserRouter>
   </Provider>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://cra.link/PWA
-//serviceWorkerRegistration.unregister();
+serviceWorkerRegistration.register();
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
--- server.js
+++ server.js
@@ -9,17 +9,23 @@ const compression = require("compression");
 const app = express();
 const port = process.env.PORT || 5000;
 
-app.use(compression());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+//app.use(cors());
 
 if (process.env.NODE_ENV === "production") {
+  app.use(compression());
   app.use(express.static(path.join(__dirname, "client/build")));
+  app.use(enforce.HTTPS({ trustProtoHeader: true }));
 
   app.get("*", function (req, res) {
     res.sendFile(path.join(__dirname, "client/build", "index.html"));
   });
 }
+
+app.get("/serviceWorkerRegistration.js", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "..", "serviceWorkerRegistration.js"));
+});
 
 app.post("/payment", (req, res) => {
   const body = {
@@ -38,7 +44,7 @@ app.post("/payment", (req, res) => {
     }
   });
 });
-
+// express-sslify
 app.listen(port, (error) => {
   if (error) throw error;
   console.log(`Server running on port: ${port}`);
