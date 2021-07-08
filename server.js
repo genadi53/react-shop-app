c02d2541e182a30e1df6de8dd9a493824ddc6bca
genadi53 <kotka5353@gmail.com>
2021-07-08 09:36:08 +0300

    add compression library

--- server.js
+++ server.js
@@ -1,47 +1,45 @@
-if(process.env.NODE_ENV !== 'production')
-require('dotenv').config();
+if (process.env.NODE_ENV !== "production") require("dotenv").config();
 
-const express = require('express');
-const path = require('path');
-const Stripe = require('stripe');
+const express = require("express");
+const path = require("path");
+const Stripe = require("stripe");
 const stripe = Stripe(process.env.STRIPE_SECRET_KEY);
+const compression = require("compression");
 
 const app = express();
 const port = process.env.PORT || 5000;
 
+app.use(compression());
 app.use(express.json());
-app.use(express.urlencoded({extended: true}));
+app.use(express.urlencoded({ extended: true }));
 
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "client/build")));
 
-if(process.env.NODE_ENV === 'production'){
-    app.use(express.static(path.join(__dirname, 'client/build')));
-    
-    app.get('*', function(req, res){
-        res.sendFile(path.join(__dirname, 'client/build', 'index.html'))
-    });
+  app.get("*", function (req, res) {
+    res.sendFile(path.join(__dirname, "client/build", "index.html"));
+  });
 }
 
-app.post('/payment', (req, res) => {
-    const body = {
-        source: req.body.token.id,
-        amount: req.body.amount,
-        //currency: 'bgn',
-        currency: 'usd',
-        description: 'Learning React'
-    };
+app.post("/payment", (req, res) => {
+  const body = {
+    source: req.body.token.id,
+    amount: req.body.amount,
+    //currency: 'bgn',
+    currency: "usd",
+    description: "Learning React",
+  };
 
-    stripe.charges.create(body, (stripeErr, stripeRes) => {
-        if(stripeErr){
-
-            res.status(500).send({ error: stripeErr });
-        } else {
-            res.status(200).send({ success: stripeRes });
-        }
-    });
-
+  stripe.charges.create(body, (stripeErr, stripeRes) => {
+    if (stripeErr) {
+      res.status(500).send({ error: stripeErr });
+    } else {
+      res.status(200).send({ success: stripeRes });
+    }
+  });
 });
 
-app.listen(port, error => {
-    if(error) throw error;
-    console.log(`Server running on port: ${port}`)
+app.listen(port, (error) => {
+  if (error) throw error;
+  console.log(`Server running on port: ${port}`);
 });
